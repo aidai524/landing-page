26be39552e4a8538e5ef45d9b8dc6203e6d94b95
xmly <xmly@xmlydeMacBook-Pro.local>
2021-10-21 16:57:54 +0800

    feat: adjust the layout for header Banner

--- src/pages/home/Banner/index.tsx
+++ src/pages/home/Banner/index.tsx
@@ -6,30 +6,43 @@ import { BlockChain, MBlockChain } from '~src/components/pic';
 import { Coins, MCoins } from '~src/components/conins';
 
 import './index.scss';
+import { MdHeight } from 'react-icons/md';
 
 const Banner = () => {
   return (
-    <div className="mx-auto w-5/6 s:w-2/3">
-      <div className="inline-flex flex-col items-end ml-32 my-32 s:items-center s:m-0 s:mb-40 s:w-full">
-        <IconNear />
-        <IconMNear />
-        <Content />
-        <MContent />
-        <button
-          className="Banner_btnLaunch l:hidden font-poppins mt-4 h-10 text-lg font-bold italic text-white rounded z-10"
-          onClick={() => window.open('https://app.ref.finance', '_blank')}
-        >
-          Launch APP
-        </button>
+    <div className="Bg_Image mx-auto justify-center flex flex-col">
+      <div className="flex justify-center  w-full">
+        <div className="Index_Header flex s:relative">
+          <div className="inline-flex flex-col items-end  my-32 s:items-center s:m-0 s:mb-40 s:w-full s:absolute" >
+            <IconNear />
+            <IconMNear />
+            <Content />
+            <MContent />
+            <button
+              className="Banner_btnLaunch l:hidden font-poppins mt-4 h-10 text-lg font-bold italic text-white rounded z-10"
+              onClick={() => window.open('https://app.ref.finance', '_blank')}
+            >
+              Launch APP
+            </button>
+          </div>
+          <BlockChain />
+          <MBlockChain />
+        </div>
+      </div>
+      <div className="flex w-full justify-center">
+        <div className="Index_Header flex s:mt-4">
+          <img src="https://i.postimg.cc/hjcL3f6W/tokens-in-banner.png" alt=""
+            style={{
+              width: '100%',
+              height: 'auto'
+            }} />
+
+        </div>
       </div>
 
-      <div className="mt-9 s:mt-24">
-        <Coins />
-        <MCoins />
-      </div>
 
-      <BlockChain />
-      <MBlockChain />
+
+      {/* <MBlockChain /> */}
     </div>
   );
 };
